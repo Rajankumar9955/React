393bdf0db2c24c670c1f354b99499adfc9d1bdf0
Rajan <krajan92946@gmail.com>
2024-08-16 01:46:31 +0530

    To Do Complete

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,31 +1,34 @@
-import { useSelector,useDispatch } from "react-redux";
 import { useState } from "react";
-import { addTask,recDelete,taskComplete,taskUnComplete,editDataSave } from "./todoSlice";
-
-const App=()=>{
-    const [txtval,setTxtval]=useState("");
+import { useSelector,useDispatch } from "react-redux";
+ import { addTask,recDelete,taskComplete,taskUnComplete,editDataSave } from "./todoSlice";
+ const App=()=>{
+    const [txtval,setTxtval]=useState("")
     const [edBtn,setEdbtn]=useState(true);
     const [edId,setEdID]=useState("");
-    const myData=useSelector((state)=>state.todo.task);
+    const mydata=useSelector((state)=>state.todo.task);
     const mydis=useDispatch();
-    console.log(myData);
+
+    console.log(mydata);
 
     let sno=0;
+    const delTask=(id)=>{
+        mydis(recDelete(id))
+    }
 
-  const delTask=(id)=>{
-    mydis(recDelete(id));
-  }
     const taskComp=(id)=>{
         mydis(taskComplete(id))
     }
+
     const taskUncomp=(id)=>{
         mydis(taskUnComplete(id))
     }
+
     const dataEdit=(id,data)=>{
         setTxtval(data)
         setEdbtn(false)
         setEdID(id)
     }
+   
     const editSave=()=>{
         mydis(editDataSave({id:edId,data:txtval}))
         setEdbtn(true)
@@ -33,51 +36,52 @@ const App=()=>{
     }
     const ans=mydata.map((key)=>{
         sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>
+                    {key.status? <span style={{color:"red",textDecoration:"line-through"}}>{key.data}</span> : key.data}
+                </td>
+                <td><button onClick={()=>{delTask(key.id)}}>Delete</button></td>
+                <td>
+                    {key.status?(
+                        <button onClick={()=>{taskUncomp(key.id)}}>Uncomplete</button>
+                    ):(
+                        <button onClick={()=>{taskComp(key.id)}}>Complete</button>
+                    )}
+                </td>
+                <td>
+                    <button onClick={()=>{dataEdit(key.id,key.data)}}>Edit</button>
+                </td>
+            </tr>
+            
+            </>
+        )
     })
     return(
         <>
-        <tr>
-            <td>{sno}</td>
-            <td>
-                {key.status? <span style={{color:"red",textDecoration:"line-through"}}>{key.data}</span>:key.data}
-            </td>
-            <td><button onClick={()=>{delTask(key.id)}}>Delete</button></td>
-            <td>
-               {key.status?(
-                <button onClick={()=>{taskUncomp(key.id)}}>Uncomplete</button>
-               ):(
-                <button onClick={()=>{taskComp(key.id)}}>complete</button>
-               )}
-            </td>
-            <td>
-                <button onClick={()=>{dataEdit(key.id,key.data)}}>Edit</button>
-            </td>
-        </tr>
+        <h1>To Do App</h1>
+        Enter Your Task: <input type="text" value={txtval} onChange={(e)=>{setTxtval(e.target.value)}} />
+        
+        {edBtn?(
+            <button onClick={()=>{mydis(addTask({id:Date.now(),data:txtval,status:false}))}}>Add</button>
+        ):(
+            <button onClick={editSave}>Edit Save</button>
+        )}
+        <hr size="4" color="red" />
+        <table border="">
+            <tr>
+                <th>SNO</th>
+                <th>Your Work</th>
+                <th>Delete</th>
+                <th>Status</th>
+                <th>Edit</th>
+            </tr>
+            {ans}
+        </table>
+        
         </>
     )
-}
-return(
-    <>
-    
-    <h1>To Do App</h1>
-    Enter Your Task:<input type="text" value={txtval} onChange={(e)=>{setTxtval(e.target.value)}} />
-    {edBtn?(
-        <button onClick={()=>{mydis(addTask({id:Date.now(),data:txtval,status:false}))}}> ADD</button>
-    ):(
-        <button onClick={()=>{editSave}}>Edit Save</button>
-    )}
-     
-     <hr size="4" color="red" />
-     <table>
-        <tr>
-            <th>#</th>
-            <th>Your Work</th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </tr>
-        {ans}
-     </table>
-    </>
-)
-export default App;
+ }
+ export default App;
--- ToDoApp/src/todoSlice.jsx
+++ ToDoApp/src/todoSlice.jsx
@@ -9,8 +9,39 @@ const todoSlice=createSlice({
         addTask:(state,action)=>{
             state.task.push(action.payload);
         },
+        recDelete:(state,actions)=>{
+            state.task=state.task.filter(item=>item.id!=actions.payload);
+        },
+        taskComplete:(state,actions)=>{
+            for(var i=0; i<state.task.length; ++i)
+            {
+                if(state.task[i].id==actions.payload)
+                {
+                    state.task[i].status=true;
+                }
+            }
+
+        },
+        taskUnComplete:(state,actions)=>{
+            for(var i=0; i<state.task.length; i++)
+            {
+                if(state.task[i].id==actions.payload)
+                {
+                    state.task[i].status=false;
+                }
+            }
+        },
+        editDataSave:(state,actions)=>{
+                 for(var i=0; i<state.task.length; i++)
+                 {
+                   if(state.task[i].id==actions.payload.id)
+                   {
+                    state.task[i].data=actions.payload.data;
+                   }
+                 }
+        }
 
     }
 })
 export default todoSlice.reducer;
-export const {addTask}=todoSlice.actions;
+export const {addTask,recDelete,taskComplete,taskUnComplete,editDataSave}=todoSlice.actions;
