54bbf8d37958d37d113fe42e37e555832f1fb07a
Rajan <krajan92946@gmail.com>
2024-08-28 15:43:06 +0530

    modify

--- Cctv Website/src/Components/Home.jsx
+++ Cctv Website/src/Components/Home.jsx
@@ -25,7 +25,7 @@ let ans=mydata.map((key)=>{
       <Card.Img variant="top" src={key.image} />
       <Card.Body>
         <Card.Title>{key.brand} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {key.name}</Card.Title>
-        <h5>{key.description}</h5> 
+        <h5 style={{color:"teal"}}>{key.description}</h5> 
         <Card.Text>
         <h3 style={{color:"green"}}>{key.price}</h3>
         </Card.Text>
@@ -48,21 +48,21 @@ let ans=mydata.map((key)=>{
       <Carousel.Item>
        <img src="https://t4techvision.com/wp-content/uploads/2024/02/2-1.jpg" alt="" style={{width:"100%",height:"500px"}}/>
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>ALL CCTV CAMERA ARE AVAILABLE</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img src="https://www.purplerockdubai.com/wp-content/uploads/2023/05/CCTV-Vs.-IP-Cameras-Which-Is-Better-image-1024x553.jpg" alt="" style={{width:"100%",height:"500px"}}/>
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3>ALL CCTV CAMERA ARE AVAILABLE</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
      <img src="https://www.clearway.co.uk/wp-content/uploads/2020/09/temp-and-fixed-800x530.jpg" alt="" style={{width:"100%",height:"500px"}}/>
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>ALL CCTV CAMERA ARE AVAILABLE</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
