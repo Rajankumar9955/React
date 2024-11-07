9e3a7aaa60c2b9e14732e691a67d79e323b2f524
Rajan <krajan92946@gmail.com>
2024-11-07 18:39:39 +0530

    Placed order section something added

--- Cctv Website/src/Components/PlacedOrder.jsx
+++ Cctv Website/src/Components/PlacedOrder.jsx
@@ -4,6 +4,7 @@
 const PlaceOrder=()=>{
     return(
         <>
+        
         <h2 align="center" style={{marginTop:"20px",color:"green"}}>Congratulation Your Order is Placed</h2>
         
         </>
