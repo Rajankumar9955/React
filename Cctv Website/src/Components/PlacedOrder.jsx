3b2c020f0cd5c25baa0838cc04e474aaa4b65c32
Rajan <krajan92946@gmail.com>
2024-11-08 13:00:12 +0530

    Placed order text align

--- Cctv Website/src/Components/PlacedOrder.jsx
+++ Cctv Website/src/Components/PlacedOrder.jsx
@@ -4,8 +4,9 @@
 const PlaceOrder=()=>{
     return(
         <>
-        
+
         <h2 align="center" style={{marginTop:"20px",color:"green"}}>Congratulation Your Order is Placed</h2>
+        
         </>
     )
 }
