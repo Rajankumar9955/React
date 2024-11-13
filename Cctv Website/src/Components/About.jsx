33958c5a06e105c50b164c6abd698fc9719ccb46
Rajan <krajan92946@gmail.com>
2024-11-13 20:26:46 +0530

    About section

--- Cctv Website/src/Components/About.jsx
+++ Cctv Website/src/Components/About.jsx
@@ -5,7 +5,6 @@ const About=()=>{
         <>
     <h2 align="center" style={{marginTop:"20px",fontSize:"30px"}}>Wel-Come To About Section</h2>
     <h1 align="center">Some Thing Special are Comming Soon...</h1>
-        
         </>
     )
 }
