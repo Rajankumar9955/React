5334ca9802affc65443df9904324e9c58872b954
Rajan <krajan92946@gmail.com>
2024-11-13 20:21:18 +0530

    Blog section

--- Cctv Website/src/Components/Blog.jsx
+++ Cctv Website/src/Components/Blog.jsx
@@ -4,7 +4,6 @@
 const Blog=()=>{
     return(
         <>
-        
        <h3 align="center" style={{marginTop:"20px"}}>Blog</h3>
        <h1 align="center" style={{color:"teal"}}>Something Special are comming sooon Please get in touch...</h1>
         
