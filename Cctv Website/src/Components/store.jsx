3d55d547ce3a78fe7e898f9a6f1ba720ab31aace
Rajan <krajan92946@gmail.com>
2024-11-27 19:13:43 +0530

    npm of persistore

--- Cctv Website/src/Components/store.jsx
+++ Cctv Website/src/Components/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import myData from "./addtocartSlice";
 
+
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
