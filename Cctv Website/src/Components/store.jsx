bee7275f241dd359066aeae1b189f8b0fb66beea
Rajan <krajan92946@gmail.com>
2024-11-27 19:14:45 +0530

    if refresh the data it's does not disappear all data

--- Cctv Website/src/Components/store.jsx
+++ Cctv Website/src/Components/store.jsx
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import myData from "./addtocartSlice";
 
-
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
